a9369af73f75377c44dcdce12716f97663e1b4c4
Slawomir1996 <slawomir.buczynski10@gmail.com>
2024-02-27 22:11:34 +0100

    fix showing profile avatar

--- api/src/user/user-controller/user.controller.ts
+++ api/src/user/user-controller/user.controller.ts
@@ -16,154 +16,150 @@ import path = require('path');
 
 
 export const storage = {
-    storage: diskStorage({
-        destination: './uploads/profileImages',
-        filename: (_req, file, cb) => {
-            const filename: string = path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4();
-            const extension: string = path.parse(file.originalname).ext;
-            
-            cb(null, `${filename}${extension}`);
-        }
-    })
-    
+  storage: diskStorage({
+    destination: './uploads/profileImages',
+    filename: (_req, file, cb) => {
+      const filename: string = path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4();
+      const extension: string = path.parse(file.originalname).ext;
+      
+      cb(null, `${filename}${extension}`);
+    }
+  })
+  
 }
 
 @Controller('users')
 export class UserController {
-    constructor(private userService: UserService) { }
+  constructor(private userService: UserService) { }
+  
+  @Get('profileImage/:imageName')
+  findProfileImage(@Param('imageName') imageName: string, @Res() res): Observable<Object> {
+    console.log(imageName);
+    return of(res.sendFile(join(process.cwd(),'./uploads/profileImage/'+imageName)));
+  }
+  @Get('/find/:username/:email')
+  findOneBYNameAndEmail(@Param('username') username: string, @Param('email') email: string): Observable<any> {
+    console.log(username);
+    return from(this.userService.findOneBYNameAndEmail(username, email)).pipe(
+      tap(user => console.log(user))
+    );
 
-@Get('/:username/:email')
-findOneBYNameAndEmail(@Param ('username') username: string, @Param('email') email: string):Observable<any>{
-    return from (this.userService. findOneBYNameAndEmail(username,email)).pipe(
-        tap(user => console.log(user))
-      );
-      
-}
+  }
 
-    @Get('unique/:username')
-    isUserNameUnique(@Param('username') username: string): Observable<boolean> {
-      return this.userService.isUserNameUnique(username);
-    };
-    @Get('unique/:email')
-    isEmailUnique(@Param('email') email: string): Observable<boolean> {
-      return this.userService.isEmailUnique(email);
-    };
-    @Get(':id')
-    findOneById(@Param('id') id: string): Observable<UserDtO> {
-        return this.userService.findOneByID(Number(id))
-    }
-    @Post('recovery-password/:username/:email')
-    updateUser(
-        @Param('username') username: string,
-        @Param('email') email: string,
-    
-      ): Observable<void> {
-        return this.userService.addTempPassword(username, email).pipe(
-          switchMap(() => {
-            // Tutaj możesz zwrócić odpowiedź, np. status HTTP 200 OK
-            return new Observable<void>((observer) => {
-              observer.next();
-              observer.complete();
-            });
-          }),
-          catchError((error) => {
-            // Tutaj obsłuż błąd, np. zwróć status HTTP 404 Not Found
-            // lub inny odpowiedni status w zależności od sytuacji
-            throw new HttpException(error.message, HttpStatus.NOT_FOUND);
-          }),
-        );
-      }
-    
+  @Get('unique/:username')
+  isUserNameUnique(@Param('username') username: string): Observable<boolean> {
+    return this.userService.isUserNameUnique(username);
+  };
+  @Get(':id')
+  findOneById(@Param('id') id: string): Observable<UserDtO> {
+ 
+    return this.userService.findOneByID(Number(id))
+  
+  }
 
-@Post()
-create(@Body() user: UserDtO): Observable<UserDtO | Object> {
+  @Post('recovery-password')
+  updateUser(@Body() body: { username: string, email: string }): Observable<void> {
+    const { username } = body;
+    const { email } = body;
+    return this.userService.addTempPassword(username, email).pipe(
+      switchMap(() => {
+        // Tutaj możesz zwrócić odpowiedź, np. status HTTP 200 OK
+        return new Observable<void>((observer) => {
+          observer.next();
+          observer.complete();
+        });
+      }),
+      catchError((error) => {
+
+        throw new HttpException(error.message, HttpStatus.NOT_FOUND);
+      }),
+    );
+  }
+
+  @Post()
+  create(@Body() user: UserDtO): Observable<UserDtO | Object> {
     return this.userService.create(user).pipe(
-        map((user: UserDtO) => user),
-        catchError(err => of({ error: err.message }))
-        );
-    }
-    
-    @Post('login')
-    login(@Body() user: UserDtO): Observable<Object> {
-        return this.userService.login(user).pipe(
-            map((jwt: string) => {
-                return { access_token: jwt };
-            })
-            )
+      map((user: UserDtO) => user),
+      catchError(err => of({ error: err.message }))
+    );
+  }
+
+  @Post('login')
+  login(@Body() user: UserDtO): Observable<Object> {
+    return this.userService.login(user).pipe(
+      map((jwt: string) => {
+        return { access_token: jwt };
+      })
+    )
+  }
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @hasRoles(UserRole.ADMIN)
+  @Get()
+  index(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+    @Query('username') username: string
+  ): Observable<Pagination<UserDtO>> {
+    limit = limit > 100 ? 100 : limit;
+
+    if (username === null || username === undefined) {
+      return this.userService.paginate({ page: Number(page), limit: Number(limit), route: 'http://localhost:3000/api/users' });
+    } else {
+      return this.userService.paginateFilterByUsername(
+        { page: Number(page), limit: Number(limit), route: 'http://localhost:3000/api/users' },
+        {
+          username,
+          items: undefined
         }
-        @UseGuards(JwtAuthGuard, RolesGuard)
-        @hasRoles(UserRole.ADMIN)
-        @Get()
-        index(
-            @Query('page') page: number = 1,
-            @Query('limit') limit: number = 10,
-            @Query('username') username: string
-            ): Observable<Pagination<UserDtO>> {
-                limit = limit > 100 ? 100 : limit;
-                
-                if (username === null || username === undefined) {
-                    return this.userService.paginate({ page: Number(page), limit: Number(limit), route: 'http://localhost:3000/api/users' });
-                } else {
-                    return this.userService.paginateFilterByUsername(
-                        { page: Number(page), limit: Number(limit), route: 'http://localhost:3000/api/users' },
-                        {
-                            username,
-                            items: undefined
-                        }
-                        )
-                    }
-                }
-                          
-                @UseGuards(JwtAuthGuard)
-                @Delete(':id')
-                deleteOne(@Param('id') id: string): Observable<any> {
-                    return this.userService.deleteOne(Number(id));
-                }
-                
-                // @UseGuards(JwtAuthGuard, UserIsUserGuard)
-                @Put(':id')
-                updateOne(@Param('id') id: string, @Body() user: UserDtO): Observable<any> {
-                    return this.userService.updateOne(Number(id), user);
-                }
-                // @UseGuards(JwtAuthGuard)
-                @Post(':id/update-password')
-                updatePassword(@Param('id') userId: number, @Body() body: {email:string,password:string, newPassword: string }): Observable<any> {
-                    const {email } = body;
-                    const {password } = body;
-                    const {newPassword } = body;
-                    return of(this.userService.validateUser(email, password)).pipe(
-                        switchMap(validatedUser => {
-                          if (validatedUser) {
-                            return this.userService.updatePassword(userId, newPassword);
-                          } else {
-                            throw new Error('Nieprawidłowy użytkownik');
-                          }
-                        })
-                      );
-                  }
-@hasRoles(UserRole.ADMIN)
-                @UseGuards(JwtAuthGuard, RolesGuard)
-                @Put(':id/role')
-                updateRoleOfUser(@Param('id') id: string, @Body() user: UserDtO): Observable<UserDtO> {
-                    return this.userService.updateRoleOfUser(Number(id), user);
-                }
-                
-                @UseGuards(JwtAuthGuard)
-                @Post('upload')
-                @UseInterceptors(FileInterceptor('file', storage))
-                uploadFile(@UploadedFile() file, @Request() req): Observable<Object> {
-                    const user: UserDtO = req.user;
-                    
-                    return this.userService.updateOne(user.id, { profileImage: file.filename }).pipe(
-                        tap((user: UserDtO) => console.log(user)),
-                        map((user: UserDtO) => ({ profileImage: user.profileImage }))
-                        )
-                    }
-                    
-                    
-                    @Get('profile-image/:imageName')
-                    findProfileImage(@Param('imageName') imageName: string, @Res() res): Observable<Object> {
-                        return of(res.sendFile(join(process.cwd(), './uploads/profileImages/' + imageName)));
-                    }
+      )
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  deleteOne(@Param('id') id: string): Observable<any> {
+    return this.userService.deleteOne(Number(id));
+  }
+
+  // @UseGuards(JwtAuthGuard, UserIsUserGuard)
+  @Put(':id')
+  updateOne(@Param('id') id: string, @Body() user: UserDtO): Observable<any> {
+    return this.userService.updateOne(Number(id), user);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Post(':id/update-password')
+  updatePassword(@Param('id') userId: number, @Body() body: { email: string, password: string, newPassword: string }): Observable<any> {
+    const { email } = body;
+    const { password } = body;
+    const { newPassword } = body;
+    return of(this.userService.validateUser(email, password)).pipe(
+      switchMap(validatedUser => {
+        if (validatedUser) {
+          return this.userService.updatePassword(userId, newPassword);
+        } else {
+          throw new Error('Nieprawidłowy użytkownik');
+        }
+      })
+    );
+  }
+  @hasRoles(UserRole.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Put(':id/role')
+  updateRoleOfUser(@Param('id') id: string, @Body() user: UserDtO): Observable<UserDtO> {
+    return this.userService.updateRoleOfUser(Number(id), user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', storage))
+  uploadFile(@UploadedFile() file, @Request() req): Observable<Object> {
+    const user: UserDtO = req.user;
+
+    return this.userService.updateOne(user.id, { profileImage: file.filename }).pipe(
+      tap((user: UserDtO) => console.log(user)),
+      map((user: UserDtO) => ({ profileImage: user.profileImage }))
+    )
+  }
+
+
 }
-                
--- api/src/user/user-services/user.service.ts
+++ api/src/user/user-services/user.service.ts
@@ -4,7 +4,7 @@ import { UserEntity } from '../model/user.entities';
 import { Like, Repository } from 'typeorm';
 import { UserDtO } from '../model/user.dto';
 import { catchError, from, map, Observable, of, switchMap, tap, throwError } from 'rxjs';
-import { AuthService} from 'src/auth/auth-services/auth.service';
+import { AuthService } from 'src/auth/auth-services/auth.service';
 import { paginate, Pagination, IPaginationOptions } from 'nestjs-typeorm-paginate';
 
 
@@ -12,81 +12,84 @@ import { paginate, Pagination, IPaginationOptions } from 'nestjs-typeorm-paginat
 
 @Injectable()
 export class UserService {
-   constructor(
-     @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>,
-      private authService: AuthService
-   ) { }
-        
+  constructor(
+    @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>,
+    private authService: AuthService
+  ) { }
+
   isUserNameUnique(username: string): Observable<boolean> {
-     return from(this.userRepository.findOne({ where: { username } })).pipe(
-       map((user) => !user) //If the answer is true be used by a user
-      )
-    }
+    return from(this.userRepository.findOne({ where: { username } })).pipe(
+      map((user) => !user) //If the answer is true be used by a user
+    )
+  }
 
   isEmailUnique(email: string): Observable<boolean> {
     return from(this.userRepository.findOne({ where: { email } })).pipe(
-       map((user) => !user) //If the answer is true be used by a user
-     )
-    }
-
-  findOneBYNameAndEmail(username: string, email: string){
-    return this.userRepository.findOne( {
-      select: ['id', 'name', 'username', 'email', 'password', 'role', 'profileImage', "tempPassword", 'isTempPasswordActive', 'tempPasswordExpirationDate'],
-         where: { username,email }})
-    }
-   generateRandomPassword(length: number): string {
-    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-      let password = '';
-        for (let i = 0; i < length; i++) {
-           const randomIndex = Math.floor(Math.random() * charset.length);
-           password += charset[randomIndex];
-          }
-          console.log(password);
-            return password;
+      map((user) => !user) //If the answer is true be used by a user
+    )
   }
-    
+
+  findOneBYNameAndEmail(username: string, email: string) {
+    return this.userRepository.findOne({
+      select: ['id', 'name', 'username', 'email', 'password', 'role', 'profileImage', "tempPassword", 'isTempPasswordActive', 'tempPasswordExpirationDate'],
+      where: { username, email }
+    })
+  }
+  generateRandomPassword(length: number): string {
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+    console.log(password);
+    return password;
+  }
+
   addTempPassword(username: string, email: string): Observable<any> {
     return from(this.findOneBYNameAndEmail(username, email)).pipe(
       switchMap((userToUpdate) => {
         if (!userToUpdate) {
-          throw new Error('Użytkownik nie istnieje');}
-      // Aktualizuj dane użytkownika
-          userToUpdate.tempPassword = this.generateRandomPassword(10); // Ustaw tymczasowe hasło
-          userToUpdate.isTempPasswordActive = true; // Ustaw flagę aktywności tymczasowego hasła
-          const expirationDate = new Date();
-          expirationDate.setDate(expirationDate.getDate() + 1); // Dodaj 1 dzień do bieżącej daty
-          userToUpdate.tempPasswordExpirationDate = expirationDate; 
-          return this.authService.hashPassword(userToUpdate.tempPassword).pipe( // Zaszyfrowanie nowego hasła
-            switchMap((hashedPassword: string) => {
-               userToUpdate.tempPassword = hashedPassword; // Ustawienie zaszyfrowanego hasła tymczasowego
-                return from(this.userRepository.save(userToUpdate)).pipe(
-                   map(() => userToUpdate));
-            })
-          );
+          throw new Error('Użytkownik nie istnieje');
+        }
+        // Aktualizuj dane użytkownika
+        userToUpdate.tempPassword = this.generateRandomPassword(10); // Ustaw tymczasowe hasło
+        userToUpdate.isTempPasswordActive = true; // Ustaw flagę aktywności tymczasowego hasła
+        const expirationDate = new Date();
+        expirationDate.setDate(expirationDate.getDate() + 1); // Dodaj 1 dzień do bieżącej daty
+        userToUpdate.tempPasswordExpirationDate = expirationDate;
+        return this.authService.hashPassword(userToUpdate.tempPassword).pipe( // Zaszyfrowanie nowego hasła
+          switchMap((hashedPassword: string) => {
+            userToUpdate.tempPassword = hashedPassword; // Ustawienie zaszyfrowanego hasła tymczasowego
+            return from(this.userRepository.save(userToUpdate)).pipe(
+              map(() => userToUpdate));
+          })
+        );
       }),
       catchError(() => throwError('Użytkownik o podanym username i email nie został znaleziony')));
   }
 
   updatePassword(userId: number, newPassword: string): Observable<any> {
-   return from(this.findOneByID(userId)).pipe( switchMap((user: UserDtO | undefined) => {
+    return from(this.findOneByID(userId)).pipe(switchMap((user: UserDtO | undefined) => {
       if (!user) {
-       throw new Error('Użytkownik o podanym ID nie istnieje.');}
-        user.isTempPasswordActive = false; // Ustaw flagę aktywności tymczasowego hasła
-        user.tempPassword=''
-        return from(this.authService.hashPassword(newPassword)).pipe(
-          switchMap((hashedPassword: string) => {
-            user.password = hashedPassword;
-            return from(this.userRepository.save(user));
-          })
-        );
-  }),
+        throw new Error('Użytkownik o podanym ID nie istnieje.');
+      }
+      user.isTempPasswordActive = false; // Ustaw flagę aktywności tymczasowego hasła
+      user.tempPassword = ''
+      return from(this.authService.hashPassword(newPassword)).pipe(
+        switchMap((hashedPassword: string) => {
+          user.password = hashedPassword;
+          return from(this.userRepository.save(user));
+        })
+      );
+    }),
       catchError((error: Error) => {
         return throwError(error);
       })
-  );
+    );
   }
 
-validateUser(email: string, password: string): Observable<UserDtO> {
+  validateUser(email: string, password: string): Observable<UserDtO> {
     return from(this.userRepository.findOne({
       select: ['id', 'name', 'username', 'email', 'password', 'role', 'profileImage', "tempPassword", 'isTempPasswordActive', 'tempPasswordExpirationDate'],
       where: {
@@ -104,7 +107,7 @@ validateUser(email: string, password: string): Observable<UserDtO> {
             return this.authService.compareTempPasswords(password, user.tempPassword).pipe(
               map((match: boolean) => {
                 if (match) {
-                  const { tempPassword,password, ...result } = user;
+                  const { tempPassword, password, ...result } = user;
                   return result;
                 } else {
                   // Handle mismatch case
@@ -129,128 +132,128 @@ validateUser(email: string, password: string): Observable<UserDtO> {
   }
 
 
-  
- create(user: UserDtO): Observable<UserDtO> {
-  return this.isUserNameUnique(user.username).pipe(
-    switchMap((isUnique) => {
-      if (isUnique) {
-        return this.authService.hashPassword(user.password).pipe(
-          switchMap((passwordHash: string) => {
-            const newUser = new UserEntity();
-            newUser.name = user.name;
-            newUser.username = user.username;
-            newUser.email = user.email;
-            newUser.password = passwordHash;
-            newUser.role = user.role;
-            newUser.profileImage = user.profileImage;
-          
-           return from(this.userRepository.save(newUser)).pipe(
-              map((user: UserDtO) => {
-                 const { password, ...result } = user;
+
+  create(user: UserDtO): Observable<UserDtO> {
+    return this.isUserNameUnique(user.username).pipe(
+      switchMap((isUnique) => {
+        if (isUnique) {
+          return this.authService.hashPassword(user.password).pipe(
+            switchMap((passwordHash: string) => {
+              const newUser = new UserEntity();
+              newUser.name = user.name;
+              newUser.username = user.username;
+              newUser.email = user.email;
+              newUser.password = passwordHash;
+              newUser.role = user.role;
+              newUser.profileImage = user.profileImage;
+
+              return from(this.userRepository.save(newUser)).pipe(
+                map((user: UserDtO) => {
+                  const { password, ...result } = user;
                   return result;
-              }),
-             catchError(err => throwError(err))
-           );
-          })
-        );
-      } else { throw new Error('User Name is busy ')}
-    })
-  );
+                }),
+                catchError(err => throwError(err))
+              );
+            })
+          );
+        } else { throw new Error('User Name is busy ') }
+      })
+    );
   }
-  
 
-    findAll(): Observable<UserDtO[]> {
-        return from(this.findAll())
-            .pipe(
-                map((users: UserDtO[]) => {
-                    users.forEach(function (v) { delete v.password });
-                    return users;
-                })
-            );
-    }
-    paginate(options: IPaginationOptions): Observable<Pagination<UserDtO>> {
-        return from(paginate<any>(this.userRepository, options)).pipe(
-            map((usersPageable: Pagination<UserDtO>) => {
-                usersPageable.items.forEach(function (v) { delete v.password });
-                return usersPageable;
-            })
-        )
-    }
 
-    paginateFilterByUsername(options: IPaginationOptions, user: UserDtO): Observable<Pagination<UserDtO>> {
-        return from(this.userRepository.findAndCount({
-            skip: Number(options.page) * Number(options.limit) || 0,
-            take: Number(options.limit) || 5,
-            order: { id: "ASC" },
-            select: ['id', 'name', 'username', 'email', 'role'],
+  findAll(): Observable<UserDtO[]> {
+    return from(this.findAll())
+      .pipe(
+        map((users: UserDtO[]) => {
+          users.forEach(function (v) { delete v.password });
+          return users;
+        })
+      );
+  }
+  paginate(options: IPaginationOptions): Observable<Pagination<UserDtO>> {
+    return from(paginate<any>(this.userRepository, options)).pipe(
+      map((usersPageable: Pagination<UserDtO>) => {
+        usersPageable.items.forEach(function (v) { delete v.password });
+        return usersPageable;
+      })
+    )
+  }
 
-            where: [
-                { username: Like(`%${user.username}%`) }
-            ]
-        })).pipe(
-            map(([users, totalUsers]) => {
-                const usersPageable: Pagination<UserDtO | any> = {
-                    items: users,
-                    links: {
-                        first: options.route + `?limit=${options.limit}`,
-                        previous: options.route + ``,
-                        next: options.route + `?limit=${options.limit}&page=${Number(options.page) + 1}`,
-                        last: options.route + `?limit=${options.limit}&page=${Math.ceil(totalUsers / Number(options.limit))}`
-                    },
-                    meta: {
-                        currentPage: Number(options.page),
-                        itemCount: users.length,
-                        itemsPerPage: Number(options.limit),
-                        totalItems: totalUsers,
-                        totalPages: Math.ceil(totalUsers / Number(options.limit))
-                    }
-                };
-                return usersPageable;
-            })
-        )
-    }
+  paginateFilterByUsername(options: IPaginationOptions, user: UserDtO): Observable<Pagination<UserDtO>> {
+    return from(this.userRepository.findAndCount({
+      skip: Number(options.page) * Number(options.limit) || 0,
+      take: Number(options.limit) || 5,
+      order: { id: "ASC" },
+      select: ['id', 'name', 'username', 'email', 'role'],
 
-    deleteOne(id: number): Observable<any> {
-        return from(this.userRepository.delete(id));
-    }
+      where: [
+        { username: Like(`%${user.username}%`) }
+      ]
+    })).pipe(
+      map(([users, totalUsers]) => {
+        const usersPageable: Pagination<UserDtO | any> = {
+          items: users,
+          links: {
+            first: options.route + `?limit=${options.limit}`,
+            previous: options.route + ``,
+            next: options.route + `?limit=${options.limit}&page=${Number(options.page) + 1}`,
+            last: options.route + `?limit=${options.limit}&page=${Math.ceil(totalUsers / Number(options.limit))}`
+          },
+          meta: {
+            currentPage: Number(options.page),
+            itemCount: users.length,
+            itemsPerPage: Number(options.limit),
+            totalItems: totalUsers,
+            totalPages: Math.ceil(totalUsers / Number(options.limit))
+          }
+        };
+        return usersPageable;
+      })
+    )
+  }
 
-    updateOne(id: number, user: UserDtO): Observable<any> {
-        delete user.email;
-        delete user.password;
-        delete user.role;
-        return from(this.userRepository.update(id, user)).pipe(
-            switchMap(() => this.findOneByID(id))
-        );
-    }
+  deleteOne(id: number): Observable<any> {
+    return from(this.userRepository.delete(id));
+  }
 
-    updateRoleOfUser(id: number, user: UserDtO): Observable<any> {
-        return from(this.userRepository.update(id, user));
-    }
+  updateOne(id: number, user: UserDtO): Observable<any> {
+    delete user.email;
+    delete user.password;
+    delete user.role;
+    return from(this.userRepository.update(id, user)).pipe(
+      switchMap(() => this.findOneByID(id))
+    );
+  }
 
-login(user: UserDtO): Observable<string> {
-        return this.validateUser(user.email, user.password).pipe(
-            tap((user: UserDtO) => console.log(user)),
-            switchMap((user: UserDtO) => {
-                if (user) {
-                    return this.authService.generateJWT(user).pipe(map((jwt: string) => jwt));
-                } else {
-                    return 'Wrong Credentials';
-                }
-            })
-        )
-    }
+  updateRoleOfUser(id: number, user: UserDtO): Observable<any> {
+    return from(this.userRepository.update(id, user));
+  }
 
-   
-    findOneByID(id: number): Observable<UserDtO> {
-        return from(this.userRepository.findOne({
-            select: ['id', 'name', 'username', 'email', 'role', 'profileImage',],
+  login(user: UserDtO): Observable<string> {
+    return this.validateUser(user.email, user.password).pipe(
+      tap((user: UserDtO) => console.log(user)),
+      switchMap((user: UserDtO) => {
+        if (user) {
+          return this.authService.generateJWT(user).pipe(map((jwt: string) => jwt));
+        } else {
+          return 'Wrong Credentials';
+        }
+      })
+    )
+  }
 
-            where: {
-                id,
-            }
-        }))
-    }
-    
+
+  findOneByID(id: number): Observable<UserDtO> {
+    return from(this.userRepository.findOne({
+      select: ['id', 'name', 'username', 'email', 'role', 'profileImage',],
+
+      where: {
+        id,
+      }
+    }))
+  }
+
 }
 
 
